51df26546f3e037448dedaae1f4f19025fd9e448
Akash Agrawal <akashagrawal01010@gmail.com>
2021-04-26 03:06:48 +0530

    feat: Add concurrency

--- ApplicationCache.go
+++ ApplicationCache.go
@@ -3,15 +3,16 @@ package main
 import (
 	"container/heap"
 	"fmt"
+	"sync"
 )
 
-var time int = 0
+var timeVar int = 0
 
 type costFunc func(int) int
 
 func myCostFunc(par int) int {
-	time = par + 1
-	return time
+	timeVar = par + 1
+	return timeVar
 }
 
 type MapValue struct {
@@ -20,17 +21,19 @@ type MapValue struct {
 }
 
 type ApplicationCache struct {
+	mu       sync.Mutex
 	size     int
 	mapStore map[string]MapValue
 	pq       *PriorityQueue
 }
 
 func (cache ApplicationCache) Add(key string, value string, myFunc costFunc) {
+	cache.mu.Lock()
 	_, ok := cache.mapStore[key]
 	if ok {
 		fmt.Printf("Key already present in the cache\n")
 	} else {
-		priori := myFunc(time)
+		priori := myFunc(timeVar)
 		item := &Item{
 			key:      key,
 			priority: priori,
@@ -44,34 +47,40 @@ func (cache ApplicationCache) Add(key string, value string, myFunc costFunc) {
 			delete(cache.mapStore, removedItem.key)
 		}
 	}
+	cache.mu.Unlock()
 }
 
 func (cache ApplicationCache) Get(key string) string {
+	cache.mu.Lock()
 	value, ok := cache.mapStore[key]
 	if ok {
 		fmt.Printf("Cache Hit!\n")
-		cache.pq.update(value.ref, value.ref.key, myCostFunc(time))
+		cache.pq.update(value.ref, value.ref.key, myCostFunc(timeVar))
+		defer cache.mu.Unlock()
 		return value.value
 	} else {
 		fmt.Printf("Cache Miss!\n")
 		//maybe throw error
+		defer cache.mu.Unlock()
 		return "-1"
 	}
 }
 
 func (cache ApplicationCache) Update(key, value string) {
+	cache.mu.Lock()
 	val, ok := cache.mapStore[key]
 	if ok {
 		cache.mapStore[key] = MapValue{value, val.ref}
-		cache.pq.update(val.ref, val.ref.key, myCostFunc(time))
+		cache.pq.update(val.ref, val.ref.key, myCostFunc(timeVar))
 		fmt.Printf("Updating %s key in cache with value %s\n", key, value)
 	} else {
 		fmt.Printf("%s key not found in cache\n", key)
 	}
-
+	cache.mu.Unlock()
 }
 
 func (cache ApplicationCache) Delete(key string) {
+	cache.mu.Lock()
 	value, ok := cache.mapStore[key]
 	if ok {
 		cache.pq.update(value.ref, value.ref.key, -1)
@@ -81,4 +90,5 @@ func (cache ApplicationCache) Delete(key string) {
 	} else {
 		fmt.Printf("Key not present in the cache\n")
 	}
+	cache.mu.Unlock()
 }
--- main.go
+++ main.go
@@ -1,24 +1,101 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"math/rand"
+	"os"
+	"time"
 )
 
-func main() {
-	fmt.Println("Hello World")
-	pq := make(PriorityQueue, 0)
-	myAppCache := ApplicationCache{4, map[string]MapValue{}, &pq}
-	myAppCache.Add("a", "1", myCostFunc)
-	myAppCache.Add("b", "2", myCostFunc)
-	myAppCache.Add("c", "3", myCostFunc)
-	myAppCache.Add("d", "4", myCostFunc)
-	myAppCache.Get("d")
-	myAppCache.Update("b", "2.2")
-	myAppCache.Delete("c")
-	fmt.Printf("%s\n", myAppCache.Get("a"))
-	fmt.Printf("%s\n", myAppCache.Get("b"))
+func randomInt(min, max int) int {
+	return min + rand.Intn(max-min)
+}
 
-	for i, s := range pq {
-		fmt.Println(i, s)
+func randomString(len int) string {
+	bytes := make([]byte, len)
+	for i := 0; i < len; i++ {
+		bytes[i] = byte(randomInt(65, 90))
+	}
+	return string(bytes)
+}
+
+func addCache(appCache *ApplicationCache, iterations int, readChan, updateChan, deleteChan chan string) {
+	fmt.Println("Inside addCache")
+	for i := 0; i < iterations; i++ {
+		key := randomString(5)
+		value := randomString(2)
+		appCache.Add(key, value, myCostFunc)
+		fmt.Printf("Added key %s with value %s \n", key, value)
+		switch randomInt(0, 2) {
+		case 0:
+			readChan <- key
+		case 1:
+			updateChan <- key
+		case 2:
+			deleteChan <- key
+		}
+		time.Sleep(50 * time.Millisecond)
+	}
+}
+
+func readCache(appCache *ApplicationCache, readChan chan string) {
+	fmt.Println("Inside readCache")
+	for i := range readChan {
+		fmt.Printf("%s key has value %s\n", i, appCache.Get(i))
+	}
+}
+
+func updateCache(appCache *ApplicationCache, updateChan chan string) {
+	fmt.Println("Inside updateCache")
+	for i := range updateChan {
+		value := randomString(2)
+		appCache.Update(i, value)
+		fmt.Printf("Update %s key with value %s\n", i, value)
+	}
+}
+
+func deleteCache(appCache *ApplicationCache, deleteChan chan string) {
+	fmt.Println("Inside deleteCache")
+	for i := range deleteChan {
+		appCache.Delete(i)
+		fmt.Printf("Deleted %s key\n", i)
+	}
+}
+
+func main() {
+	pq := make(PriorityQueue, 0)
+	myAppCache := ApplicationCache{
+		size:     4,
+		mapStore: map[string]MapValue{},
+		pq:       &pq}
+	// myAppCache.Add("a", "1", myCostFunc)
+	// myAppCache.Add("b", "2", myCostFunc)
+	// myAppCache.Add("c", "3", myCostFunc)
+	// myAppCache.Add("d", "4", myCostFunc)
+	// myAppCache.Get("d")
+	// myAppCache.Update("b", "2.2")
+	// myAppCache.Delete("c")
+	// fmt.Printf("%s\n", myAppCache.Get("a"))
+	// fmt.Printf("%s\n", myAppCache.Get("b"))
+
+	// for i, s := range pq {
+	// 	fmt.Println(i, s)
+	// }
+	readChan := make(chan string, 10)
+	updateChan := make(chan string, 10)
+	deleteChan := make(chan string, 10)
+	iterations := 10
+	go addCache(&myAppCache, iterations, readChan, updateChan, deleteChan)
+	go readCache(&myAppCache, readChan)
+	go updateCache(&myAppCache, updateChan)
+	go deleteCache(&myAppCache, deleteChan)
+	// time.Sleep(time.Duration(iterations*100) * time.Millisecond)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		s := scanner.Text()
+		if s == "exit" {
+			return
+		}
 	}
 }
